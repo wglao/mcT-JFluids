d1955b929aef9394d15cf4ef882a31cc6836cd4a
wglao <wesleygarlao@gmail.com>
2022-11-22 16:32:55 -0600

    removed redundant examples folder

--- examples/examples_1D/01_linearadvection/run_linearadvection.py
+++ examples/examples_1D/01_linearadvection/run_linearadvection.py
@@ -3,24 +3,38 @@ from jaxfluids import InputReader, Initializer, SimulationManager
 from jaxfluids.post_process import load_data, create_lineplot
 
 # SETUP SIMULATION
-input_reader = InputReader("linearadvection.json", "numerical_setup.json")
-initializer  = Initializer(input_reader)
-sim_manager  = SimulationManager(input_reader)
+def setup(problem, numerical):
+    input_reader = InputReader(problem, numerical)
+    initializer  = Initializer(input_reader)
+    sim_manager  = SimulationManager(input_reader)
+    return input_reader, initializer, sim_manager
 
 # RUN SIMULATION
-buffer_dictionary = initializer.initialization()
-sim_manager.simulate(buffer_dictionary)
+def sim(initializer, sim_manager):
+    buffer_dictionary = initializer.initialization()
+    sim_manager.simulate(buffer_dictionary)
+    return buffer_dictionary, sim_manager
 
 # LOAD DATA
-path = sim_manager.output_writer.save_path_domain
-quantities = ["density"]
-cell_centers, cell_sizes, times, data_dict = load_data(path, quantities)
+def load(sim_manager):
+    path = sim_manager.output_writer.save_path_domain
+    quantities = ["density"]
+    cell_centers, cell_sizes, times, data_dict = load_data(path, quantities)
+    return cell_centers, cell_sizes, times, data_dict
 
 # PLOT
-nrows_ncols = (1,1)
-create_lineplot(data_dict, cell_centers, times, nrows_ncols=nrows_ncols, interval=100)
+def plot(cell_centers, times, data_dict):
+    nrows_ncols = (1,1)
+    create_lineplot(data_dict, cell_centers, times, nrows_ncols=nrows_ncols, interval=100)
 
-fig, ax = plt.subplots()
-ax.plot(cell_centers[0], data_dict["density"][-1,:,0,0])
-ax.plot(cell_centers[0], data_dict["density"][0,:,0,0], color="black")
-plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(cell_centers[0], data_dict["density"][-1,:,0,0])
+    # ax.plot(cell_centers[0], data_dict["density"][1,:,0,0], '--')
+    ax.plot(cell_centers[0], data_dict["density"][0,:,0,0], color="black")
+    plt.show()
+
+if __name__ == "__main__":
+    input_reader, initializer, sim_manager = setup("linearadvection.json", "numerical_setup.json")
+    buffer_dictionary, sim_manager = sim(initializer, sim_manager)
+    cell_centers, cell_sizes, times, data_dict = load(sim_manager)
+    plot(cell_centers, times, data_dict)
